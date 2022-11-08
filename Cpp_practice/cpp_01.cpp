16ad312e31542317a80c36747b94c0b94fa7637a
허윤우 <hyoon516@naver.com>
2022-11-08 17:46:10 +0900

    c++에서 반복문 안에 복합대입연산자를 사용한 예제

--- Cpp_practice/cpp_01.cpp
+++ Cpp_practice/cpp_01.cpp
@@ -1,20 +1,21 @@
 #include <iostream>
-#include <string>   //String은 일반적인 자료형이 아니라 클래스이기 때문에 전처리과정이 필요.
 
 using namespace std;
 
 int main() {
 
-	int x = 10;
-	int y = 2;
+	int sum1 = 0;
+	int sum2 = 0;
 
-	int sum = x + y;
+	int one = 1;
+	int two = 2;
 
-	if (x < y)
-		cout << "x가 y보다 작습니다." << endl;
-	else
-		cout << "y가 x보다 작습니다." << endl;
+	for (int i = 0; i < 5; i++) {
+		sum1 += one;
+		sum2 += two;
+	}
 
+	cout << "합산 결과: " << sum1 << "," << sum2 << endl;
 
 	return 0;
 }
