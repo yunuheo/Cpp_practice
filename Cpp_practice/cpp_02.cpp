71844b6b8ab30a2c04803b4596e93f8d202a9d66
허윤우 <hyoon516@naver.com>
2022-11-12 19:46:56 +0900

    cpp에서 조건부 삼항 연산자 이해하기

---
+++ Cpp_practice/cpp_02.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	const int ARRAY_SIZE = 3;
+
+	int founding[ARRAY_SIZE];
+	founding[0] = 918;
+	founding[1] = 1392;
+	founding[2] = 1948;
+
+	cout << "고려 건국연도: " << founding[0] << endl;
+	cout << "조선 건국연도: " << founding[1] << endl;
+	cout << "한국 건국연도: " << founding[2] << endl;
+
+	return 0;
+}
