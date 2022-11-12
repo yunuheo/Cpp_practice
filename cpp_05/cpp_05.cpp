54305188c6e70ef75e1a69eac651b349b3f70347
허윤우 <hyoon516@naver.com>
2022-11-12 20:40:21 +0900

    비트연산자 이해하기 -<bitset>라이브러리 사용 - reset()으로 모든 비트수를 0으로 초기화 - 10진수를 변수 안에 넣으면 자동으로 2진수 비트로 변환

---
+++ cpp_05/cpp_05.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <bitset>
+
+using namespace std;
+
+int main() {
+
+	bitset<8> bit1;
+	bit1.reset(); // 0000 0000
+	bit1 = 126; // 0111 1111
+
+	bitset<8> bit2;
+	bit2.reset();
+	bit2 = 0x20; // 32
+	
+	bitset<8> bit3 = bit1 & bit2;
+	bitset<8> bit4 = bit1 | bit2;
+	bitset<8> bit5 = bit1 ^ bit2;
+	bitset<8> bit6 = ~bit1;
+	bitset<8> bit7 = bit2 << 1;
+	bitset<8> bit8 = bit2 >> 1;
+
+	cout << "================위에 결과를 순서대로 출력===================" << endl;
+	cout << bit3 << "," << bit3.to_ulong() << endl;
+	cout << bit4 << "," << bit4.to_ulong() << endl;
+	cout << bit5 << "," << bit5.to_ulong() << endl;
+	cout << bit6 << "," << bit6.to_ulong() << endl;
+	cout << bit7 << "," << bit7.to_ulong() << endl;
+	cout << bit8 << "," << bit8.to_ulong() << endl;
+
+
+	return 0;
+}
