4500da6d0b26e7396fc5dabd49cd6a79c99a3bfb
허윤우 <hyoon516@naver.com>
2022-11-26 20:55:04 +0900

    이중 for문으로 피보나치 수열 구현하기

---
+++ cpp_06/cpp_06.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+//피보나치 수열 구현하기
+int main() {
+
+	int a = 0;
+	int b = 0;
+	int c = 0;
+
+	for (int i = 1; i < 10; i++)
+	{
+		a = 0;
+		b = 1;
+		for (int j = 1; j <= i; j++) {
+
+			cout << b << ",";
+
+			c = b;
+			b = b + a;
+			a = c;
+			
+		}
+		cout << endl;
+	}
+
+	return 0;
+}
